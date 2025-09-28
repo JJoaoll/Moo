cb55e7c5b2c6ef03adfd024022f69a7f8b16d8e4
JJoaoll <joaoduos@gmail.com>
2025-09-28 19:27:02 -0300

    refactor: add: constructor as literals and expressions

--- src/Grammar/Expr.hs
+++ src/Grammar/Expr.hs
@@ -6,14 +6,25 @@ import Utils
 
 -- import Data.List.NonEmpty
 
+-- l = [1, 2]
+-- l = 1::2::[]
+-- ELit $ LConstr
+-- LConstr "Cons" [1, LConstr "Cons" [2, LConstr "Nil" []]]
+-- "True" --> ELit (LConstr "True" [])
+
+-- EConstr
+-- LConstr "Cons" [1, LConstr "Cons" [x, LConstr "Nil" []]]
+
+
 data Lit
   = LInt   Int 
   | LChar  Char
   | LFloat Float
+  | LConstr Name [Lit]
   deriving (Eq, Show)
 
 data Expr
-  = ELit Lit
+  = ELit Lit | EConstr Name [Expr]
   | EVar Name | EConst Name | EGlobal Name -- optimal
   | EUnOp UnOp Expr | EBinOp Expr BinOp Expr
   | EFunCall Name [Expr]
